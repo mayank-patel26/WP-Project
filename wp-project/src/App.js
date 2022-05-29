500fd88da48362327307a18382f6efa66f710a86
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2022-05-29 18:27:09 +0530

    Removed spaces

--- wp-project/src/App.js
+++ wp-project/src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import MyNavbar from './components/MyNavbar';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Product from './components/Product';
 import Login from './components/Login';
-
 import Cart from './components/Cart';
 
 function App() {
   return (
-
     <BrowserRouter>
-    
     <MyNavbar/>
-
     <div className="App">
       <Routes>
         <Route  path='/' element={<Login/>}/>
