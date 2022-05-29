90c3920715061bb2300e499ba9110e701be9a95d
Tanmai Kamat <tanmaikamat@gmail.com>
2022-05-29 13:50:02 +0530

    New Routes added

--- wp-project/src/components/Navbar.js
+++ wp-project/src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export default function Navbar() {
   return (
     <div>
-        
+    
+    <Link to='/products'>Products</Link>
+    
+    <Link to='/'>Home</Link>
     </div>
   )
 }
+
