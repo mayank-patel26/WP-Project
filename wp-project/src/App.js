d57c259c2a8d5737167a2ffbc726fca1ff5f8c54
Tanmai Kamat <tanmaikamat@gmail.com>
2022-05-29 13:49:51 +0530

    New Routes added

--- wp-project/src/App.js
+++ wp-project/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Product from './components/Product';
+import Login from './components/Login';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      
+      <Navbar/>
+      <Routes>
+        <Route  path='/' element={<Login/>}/>
+        <Route  path='/home' element={<Home/>}/>
+        <Route path='/products' element={<Product />}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
