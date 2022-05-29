e791f500125ee2637c07fb6bdf9debfa961828d1
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2022-05-29 13:42:55 +0530

    Made some changes

--- wp-project/src/App.js
+++ wp-project/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import MyNavbar from './components/MyNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
-      <Navbar/> 
+      <MyNavbar/> 
     </div>
   );
 }
