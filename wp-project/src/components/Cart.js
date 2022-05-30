71d4593acf697c3b426e0af197c76290512606cd
Mayank Patel <mayank509.mp@gmail.com>
2022-05-30 22:02:43 +0530

    Update Cart.js

--- wp-project/src/components/Cart.js
+++ wp-project/src/components/Cart.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
 
 export default function Cart() {
   return (
-    <div>Cart</div>
-  )
+    <Container fluid className="mt-4">
+      <h2 className="display-4 fw-bold py-4">Cart</h2>
+      <Row xs={1} sm={2} md={4}>
+        <Col xs={1} sm={1} md={2} lg={2} className=" flex-grow-1">
+          <Card
+            style={{ width: "30%", height: "20rem", cursor: "pointer", border: "0" }}
+            className="m-2"
+          >
+            <Card.Img
+              variant="top"
+              height={250}
+              width={50}
+              src="http://brandingbypixels.com/wp-content/uploads/2017/11/1.-Branding-by-Pixels.png"
+              style={{display: "inline"}}
+            />   
+            <Card.Body>
+              <div className = "d-flex">
+                <Button variant="light" className="fw-5">+</Button>
+                <Button variant="light">-</Button>
+              </div>
+            </Card.Body>
+       
+        
+        
+          
+          
+          
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
