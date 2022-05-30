06f74194c1f202a2a6ca14493d90fbf9a038acf0
Tanmai Kamat <tanmaikamat@gmail.com>
2022-05-31 04:01:54 +0530

    Basic of ProductItem Created

--- wp-project/src/components/ProductItem.js
+++ wp-project/src/components/ProductItem.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React,{useState,useEffect} from 'react'
+import { Container,Row,Image,Col } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom'
+import { getProduct } from '../api/serverApi';
 
-export default function ProductItem(props) {
+const getP=async (id,func)=>{
+let item=await axios.get(getProduct(id));
+console.log(item['data']['products'].filter((i)=>(i._id===id))[0]);
+func(item['data']['products'].filter((i)=>(i._id===id))[0]);
+}
+export default function ProductItem() {
   const {id}=useLocation().state;
+  const [Item, setItem] = useState({});
+  useEffect(() => {
+    
+  getP(id,setItem);
+  }, [])
+  
   return (
-
-    <div>{id} hi</div>
+    <Container>
+      <Row>
+        <Image
+        src={Item.img}
+        width={500}
+        height={500}
+        className='m-3'/>
+      </Row>
+      <Row >
+        <Col className='d-flex flex-column p-3'>
+          
+        <h3>{Item.title}</h3>
+        <h4>Rs .{Item.price}</h4>
+        </Col>
+      </Row>
+    </Container>
   )
 }
